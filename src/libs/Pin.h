750b6024f4b77bd2d6859dfeae98a7f87303f160
acurana <christopher.lang@acurana.de>
2016-10-02 20:22:22 +0200

    FABBSTER: modified pin to support multiplexer

--- src/libs/Pin.h
+++ src/libs/Pin.h
@@ -13,6 +13,9 @@ namespace mbed {
     class InterruptIn;
 }
 
+class Multiplexer;
+
+
 class Pin {
     public:
         Pin();
@@ -51,16 +54,23 @@ class Pin {
 
         inline bool get(){
             if (!this->valid) return false;
-            return this->inverting ^ (( this->port->FIOPIN >> this->pin ) & 1);
+            if (! multiplexed)
+                return this->inverting ^ (( this->port->FIOPIN >> this->pin ) & 1);
+            else
+                return get_multiplexed();
         }
 
         inline void set(bool value)
         {
             if (!this->valid) return;
-            if ( this->inverting ^ value )
-                this->port->FIOSET = 1 << this->pin;
+            if (! multiplexed) {
+                if ( this->inverting ^ value )
+                    this->port->FIOSET = 1 << this->pin;
+                else
+                    this->port->FIOCLR = 1 << this->pin;
+            }
             else
-                this->port->FIOCLR = 1 << this->pin;
+                set_multiplexed(value);
         }
 
         mbed::PwmOut *hardware_pwm();
@@ -75,10 +85,39 @@ class Pin {
         struct {
             bool inverting:1;
             bool valid:1;
+            bool multiplexed:1;
         };
+
+        void multiplexed_pin(char const* cp);	// for "from_string"
+
+        bool get_multiplexed(void);				// read pin
+        void set_multiplexed(bool value);       // write pin
+
+        uint8_t get_mpx_index(void);            // returns the multiplexer channel for that pin
+        //void set_mpx_index(uint8_t);
+
+        //void set_mpx_channel(void);				// apply mpx channel on the pins
+
+        bool is_multiplexed(void);				// check if pin is mpxed
+
+        //unsigned get_mpx_num_chan(void);
+
+        uint16_t get_adc_ch(void);
+        void set_adc_ch(uint16_t ch);
+
+        Multiplexer* get_mpx_ptr(void);
+
+    private:
+        /*
+         * a pin may be multiplexed via analogue switch or shift register
+         */
+        Multiplexer* mpx;
+        uint8_t idx;        // position in mpxer
+        uint16_t  adc_ch;   // the corresponding adc channel
 };
 
 
 
 
 #endif
+
